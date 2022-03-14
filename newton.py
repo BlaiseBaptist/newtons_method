e385b19c265c63ce97777df9bc74663cde37f4b8
BlaiseBaptist <40903991+BlaiseBaptist@users.noreply.github.com>
2022-03-14 19:56:54 -0400

    intial commit

---
+++ newton.py
@@ -0,0 +1,69 @@
+from time import sleep
+import matplotlib.pyplot as plt
+import numpy as np
+import cmath as bmath
+
+class plot():
+  def __init__(self,fig,ax):
+      self.fig = fig
+      self.ax = ax
+      self.scatter = make_plot([-10,10],[-10,10])
+  
+  def resize(self,event):
+    print('clicked')
+    self.scatter.remove()
+    self.scatter = make_plot(self.ax.get_xbound(),self.ax.get_ybound())
+
+
+def f(x):
+  return(x**3-x**2+x-1)
+  
+
+
+def f_prime(x):
+  return(3*(x**2)-2*x+1)
+
+
+def newtons_method(x):
+  while True:
+    f_p=f_prime(x) 
+    fn = f(x)
+    if abs(fn)<= .00001:
+      return(x)
+    if f_p == 0:
+      return(0)
+    if abs(fn) > 10000:
+      return(0)
+    x=x-fn/f_p
+
+def find_roots():
+  return([0,1,complex(0,1),complex(0,-1)])
+
+
+def colorize(y,roots):
+  return(['white','red','green','blue'][min_index(list(map(lambda root:abs(root-y),roots)))])
+
+
+def min_index(jimbo):
+  return(jimbo.index(min(jimbo)))
+
+
+def make_plot(x,y):
+  x = np.arange(x[0],x[1],(x[1]-x[0])/400)
+  y = np.arange(y[0],y[1],(y[1]-y[0])/400)
+  x1,x2 = np.meshgrid(x,y)
+  cx = list(map(complex,x1.flatten(),x2.flatten()))
+  roots = [find_roots()]*len(cx)
+  root = list(map(newtons_method,cx)) 
+  c = list(map(colorize,root,roots))
+  return(plt.scatter(x1.flatten(),x2.flatten(),c = c,s = 1,marker = '.'))
+
+
+def main():
+  fig,ax = plt.subplots()
+  p = plot(fig,ax)
+  p.fig.canvas.mpl_connect('button_release_event',p.resize)
+  plt.show()
+
+
+main()
